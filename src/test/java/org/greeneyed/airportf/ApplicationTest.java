268d615bfca2642ce9b5c64b5c04fd455ecc600c
verdoso <Verdoso@users.noreply.github.com>
2021-06-16 19:21:12 +0200

    Added MockBean to test, otherwise it won't start without credentials

--- src/test/java/org/greeneyed/airportf/ApplicationTest.java
+++ src/test/java/org/greeneyed/airportf/ApplicationTest.java
@@ -2,12 +2,20 @@ package org.greeneyed.airportf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.maxmind.geoip2.WebServiceClient;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private WebServiceClient webServiceClient;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
